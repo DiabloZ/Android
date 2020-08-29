239b7739bf6f31db92c344dc7f6de7e2fa3f2616
DiabloZ <DiabloZ@me.com>
2020-08-29 11:42:20 +0300

    Недокоммитилось?

--- app/example_1/src/main/java/com/example/example_1/MainActivity.java
+++ app/example_1/src/main/java/com/example/example_1/MainActivity.java
@@ -10,9 +10,9 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    final String LOG_TAG = "Main_logs";
+    private final String LOG_TAG = "main_logs";
 
-    private EditText etSendToSecondActivity;
+    private EditText sendToEditText;
     private Button btnSend;
 
     @Override
@@ -23,24 +23,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         initElem();
     }
 
-    private void initElem() {
-        etSendToSecondActivity = findViewById(R.id.etSendToSecondActivity);
-        btnSend = findViewById(R.id.btnSend);
-
-        btnSend.setOnClickListener(this);
-        Log.d(LOG_TAG, "Элементы инизиализированы");
-    }
-
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btnSend:
-                String sendText = etSendToSecondActivity.getText().toString();
-                SecondActivity.startActivity(this, sendText);
+                String sendText = sendToEditText.getText().toString();
+                startActivity(SecondActivity.startActivity(this, sendText));
                 Log.d(LOG_TAG, "btnSend was clicked");
                 return;
             default:
                 return;
         }
+    }
+
+    private void initElem() {
+        sendToEditText = findViewById(R.id.sendToEditText);
+        btnSend = findViewById(R.id.btnSend);
+
+        btnSend.setOnClickListener(this);
+        Log.d(LOG_TAG, "Элементы инизиализированы");
     }
 }
--- app/example_1/src/main/java/com/example/example_1/SecondActivity.java
+++ app/example_1/src/main/java/com/example/example_1/SecondActivity.java
@@ -2,23 +2,23 @@ package com.example.example_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
-    final String LOG_TAG = "Second_logs";
-    final String[] EMAIL = {"test@test.com", "1@2.ra"};
-    final String SUBJECT = "me?";
-    final String ATTACHMENT = "testAttachment";
+    private final String LOG_TAG = "second_logs";
+    private final String SUBJECT = "me?";
+    private final String ATTACHMENT = "testAttachment";
 
-    private TextView tvSecondGetMessage;
-    private Button   btnSecondSendEmail;
+    private final String[] EMAIL = {"test@test.com", "1@2.ra"};
+
+    private TextView secondTextView;
+    private Button secondButtonSendEmail;
 
     private String textFromFirstActivity;
 
@@ -29,35 +29,9 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         initElem();
-        getDataFromIntent();
-        useDataFromIntent();
-
+        textFromFirstActivity =  getIntent().getStringExtra(KEY_SECOND);
+        secondTextView.setText(textFromFirstActivity);
     }
-
-    private void useDataFromIntent() {
-        tvSecondGetMessage.setText(textFromFirstActivity);
-    }
-
-    private void getDataFromIntent() {
-        Intent intent = getIntent();
-        textFromFirstActivity = intent.getStringExtra(KEY_SECOND);
-    }
-
-
-    private void initElem() {
-        tvSecondGetMessage = findViewById(R.id.tvSecondGetMessage);
-        btnSecondSendEmail = findViewById(R.id.btnSecondSendEmail);
-
-        btnSecondSendEmail.setOnClickListener(this);
-        Log.d(LOG_TAG, "Элементы инизиализированы");
-    }
-
-    public static void startActivity(Activity activity, String message) {
-        Intent intent = new Intent(activity, SecondActivity.class);
-        intent.putExtra(KEY_SECOND, message);
-        activity.startActivity(intent);
-    }
-
 
     @Override
     public void onClick(View view) {
@@ -70,13 +44,30 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
         }
     }
 
+    public static Intent startActivity(Context context, String message) {
+        return new Intent(context, SecondActivity.class)
+                .putExtra(KEY_SECOND, message);
+    }
+
+
+
+
+    private void initElem() {
+        secondTextView = findViewById(R.id.tvSecondGetMessage);
+        secondButtonSendEmail = findViewById(R.id.btnSecondSendEmail);
+
+        secondButtonSendEmail.setOnClickListener(this);
+        Log.d(LOG_TAG, "Элементы инизиализированы");
+    }
+
+
     private void sendMailFromIntent() {
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("*/*");
-        intent.putExtra(Intent.EXTRA_EMAIL, EMAIL);
-        intent.putExtra(Intent.EXTRA_SUBJECT, SUBJECT);
-        intent.putExtra(Intent.EXTRA_STREAM, ATTACHMENT);
-        intent.putExtra(Intent.EXTRA_TEXT, tvSecondGetMessage.getText().toString());
+            intent.putExtra(Intent.EXTRA_EMAIL, EMAIL);
+            intent.putExtra(Intent.EXTRA_SUBJECT, SUBJECT);
+            intent.putExtra(Intent.EXTRA_STREAM, ATTACHMENT);
+            intent.putExtra(Intent.EXTRA_TEXT, secondTextView.getText().toString());
         Log.d(LOG_TAG, EMAIL[0]);
         if(intent.resolveActivity(getPackageManager()) != null) startActivity(intent);
     }
