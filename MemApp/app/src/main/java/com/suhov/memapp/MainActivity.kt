cabe10b5d563f587dc9d81786cf919390bf6480a
Сухов Виталий <DiabloZ@me.com>
2021-01-08 21:09:39 +0300

    Completed 3 example withMith = Row Layouts

--- MemApp/app/src/main/java/com/suhov/memapp/MainActivity.kt
+++ MemApp/app/src/main/java/com/suhov/memapp/MainActivity.kt
@@ -3,11 +3,10 @@ package com.suhov.memapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.ScrollableColumn
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.*
 import androidx.compose.ui.graphics.Color
@@ -31,22 +30,95 @@ class MainActivity : AppCompatActivity() {
     @Composable
     fun TestTest(){
         Example3()
+        //Example3test()
         //Example2()
+        //Example1()
     }
 
     @Composable
-    fun Example3(){
+    fun Example3() {
         ScrollableColumn(
                 modifier = Modifier
                         .fillMaxSize()
                         .background(color = Color.LightGray)
         ) {
             Image(
-                bitmap = imageFromResource(
-                    res = resources,
-                    resId = R.drawable.robo_vacuum_cleaner
-                ),
-                modifier = Modifier.height(300.dp),
+                    bitmap = imageFromResource(
+                            res = resources,
+                            resId = R.drawable.robo_vacuum_cleaner
+                    ),
+                    modifier = Modifier.height(300.dp),
+                    contentScale = ContentScale.Crop,
+            )
+            Column(modifier = Modifier.padding(16.dp)
+            ){
+                Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                ){
+                    Text(
+                            text = "Robo",
+                            style = TextStyle(
+                                    fontSize = 26.sp))
+                    Spacer(modifier = Modifier.padding(top = 10.dp))
+                    Text(text = "$150.00",
+                        style = TextStyle(
+                            fontSize = 17.sp,
+                            color = Color(0xFF00C853)),
+                        modifier =  Modifier.align(Alignment.CenterVertically)
+                            )
+                }
+                Spacer(modifier = Modifier.padding(top = 10.dp))
+                Text(text = "2000 Pa")
+                Spacer(modifier = Modifier.padding(top = 10.dp))
+                Button(onClick = {},
+                modifier = Modifier.align(Alignment.CenterHorizontally)) {
+                Text(text = "Order now!")
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun Example3test(){
+        Column {
+            Column(modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .border(border = BorderStroke(width = 1.dp, color = Color.Black)),
+                    verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = "Item 1",
+                        modifier = Modifier.align(Alignment.CenterHorizontally))
+                Text(text = "Item 2",
+                        modifier = Modifier.align(Alignment.CenterHorizontally))
+            }
+            Spacer(modifier = Modifier.padding(20.dp))
+            Row(modifier = Modifier
+                    .width(200.dp)
+                    .height(200.dp)
+                    .border(border = BorderStroke(width = 1.dp, color = Color.Black)),
+                    horizontalArrangement = Arrangement.Center
+            ) {
+                Text(text = "Item 3",
+                        modifier = Modifier.align(Alignment.CenterVertically))
+            }
+        }
+    }
+
+    @Composable
+    fun Example2(){
+        ScrollableColumn(
+                modifier = Modifier
+                        .fillMaxSize()
+                        .background(color = Color.LightGray)
+        ) {
+            Image(
+                    bitmap = imageFromResource(
+                            res = resources,
+                            resId = R.drawable.robo_vacuum_cleaner
+                    ),
+                    modifier = Modifier.height(300.dp),
                     contentScale = ContentScale.Crop,
             )
             Column(modifier = Modifier.padding(16.dp)
@@ -60,14 +132,14 @@ class MainActivity : AppCompatActivity() {
                 Spacer(modifier = Modifier.padding(top = 10.dp))
                 Text(text = "$150.00", style = TextStyle(
                         fontSize = 17.sp,
-                color = Color(0x81AA00FF)))
+                        color = Color(0x81AA00FF)))
 
             }
         }
     }
 
     @Composable
-    fun Example2(){
+    fun Example1(){
         Column(
                 modifier = Modifier.padding(16.dp)
         ) {
